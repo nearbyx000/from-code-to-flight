2bd98b9a10cd2a5423920d47aebff9f23ff4f310
nearbyx000 <misterpipka881@gmail.com>
2025-10-27 16:40:17 +0300

    testing

--- mav_with_local.py
+++ mav_with_local.py
@@ -1,4 +1,4 @@
-#!/usr/-bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import rospy
@@ -7,17 +7,13 @@ from mavros_msgs.srv import CommandBool, SetMode, CommandTOL
 from mavros_msgs.msg import State
 import math
 
-# ПРЕДПОЛАГАЕТСЯ, что сервис для светодиодов - это стандартный SetBool
-# Замените 'set_led' на имя реального сервиса из документации олимпиады
-from std_srvs.srv import SetBool 
-
 class DroneController:
     def __init__(self):
-        rospy.init_node('fly_robotics_controller')
+        rospy.init_node('fly_robotics_mvp_controller')
 
-        # --- 1. Координаты и маршрут (из вашего запроса) ---
+        # --- 1. Координаты и маршрут (из нашего обсуждения) ---
         self.coordinates = {
-            '0': (0.0, 0.0, 1.0),  # H (База) - всегда на Z=1 для полета
+            '0': (0.0, 0.0, 1.0),  # H (База) - рабочая высота 1м 
             'A': (2.5, 1.0, 1.0),
             'B': (6.5, 1.0, 1.0),
             '1': (0.5, 3.0, 1.0),
@@ -27,8 +23,14 @@ class DroneController:
             '5': (4.5, 5.5, 1.0)
         }
         
-        # Исправленный, корректный маршрут
-        self.route_plan = ['B', '1', 'B', '5', 'B', '2', 'A', '4', 'A', '3']
+        # Корректный маршрут, выполняющий все 5 доставок [cite: 49, 50]
+        self.route_plan = [
+            'B', '1',  # Доставка 1 (B -> 1)
+            'B', '5',  # Доставка 2 (B -> 5)
+            'B', '2',  # Доставка 3 (B -> 2)
+            'A', '4',  # Доставка 4 (A -> 4)
+            'A', '3'   # Доставка 5 (A -> 3)
+        ]
         
         self.pickup_points = ['A', 'B']
         self.dropoff_points = ['1', '2', '3', '4', '5']
@@ -36,16 +38,17 @@ class DroneController:
         # --- 2. Настройка ROS ---
         self.current_state = None
         self.current_pose = None
-        self.rate = rospy.Rate(20.0)
+        self.rate = rospy.Rate(20.0) # 20 Гц для MAVROS
 
         # Подписчики
         rospy.Subscriber('/mavros/state', State, self.state_callback)
         rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.pose_callback)
 
-        # Публикаторы
+        # Публикатор
         self.setpoint_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
 
-        # Клиенты сервисов
+        # Клиенты сервисов MAVROS
+        rospy.loginfo("Ожидание сервисов MAVROS...")
         rospy.wait_for_service('/mavros/cmd/arming')
         self.arm_service = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
         
@@ -54,14 +57,9 @@ class DroneController:
 
         rospy.wait_for_service('/mavros/cmd/land')
         self.land_service = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
+        rospy.loginfo("Сервисы MAVROS подключены.")
 
-        # Сервис светодиодов (!! ЗАМЕНИТЬ НА РЕАЛЬНЫЙ СЕРВИС !!)
-        try:
-            rospy.wait_for_service('set_led', timeout=2.0)
-            self.set_led_service = rospy.ServiceProxy('set_led', SetBool)
-        except rospy.ROSException:
-            rospy.logwarn("Сервис 'set_led' не найден. Имитация работы.")
-            self.set_led_service = None
+        # MVP: Сервис светодиодов пропущен
 
         # --- 3. Подготовка к полету ---
         self.prepare_flight()
@@ -71,18 +69,6 @@ class DroneController:
 
     def pose_callback(self, msg):
         self.current_pose = msg
-
-    def set_led(self, state):
-        """Управляет светодиодной лентой."""
-        if self.set_led_service is None:
-            rospy.loginfo(f"[ИМИТАЦИЯ] LED: {'ЗЕЛЕНЫЙ' if state else 'ВЫКЛ'}")
-            return
-            
-        try:
-            # True = зеленый [cite: 23], False = выкл [cite: 25]
-            self.set_led_service(state) 
-        except rospy.ServiceException as e:
-            rospy.logerr(f"Ошибка вызова сервиса LED: {e}")
 
     def is_at_position(self, target_coords, tolerance=0.3):
         """Проверяет, достиг ли дрон цели с допуском."""
@@ -98,7 +84,7 @@ class DroneController:
         return dist < tolerance
 
     def go_to_point(self, point_name):
-        """Летит к точке и ждет ее достижения."""
+        """Летит к точке по ее имени и ждет ее достижения."""
         target_coords = self.coordinates[point_name]
         pose = PoseStamped()
         pose.header.stamp = rospy.Time.now()
@@ -108,7 +94,10 @@ class DroneController:
         
         rospy.loginfo(f"Движение к {point_name} {target_coords}...")
         
-        while not self.is_at_position(target_coords) and not rospy.is_shutdown():
+        while not rospy.is_shutdown():
+            if self.is_at_position(target_coords):
+                break
+                
             if (self.current_state and self.current_state.mode != "OFFBOARD"):
                 rospy.logwarn("Режим OFFBOARD потерян! Попытка восстановления...")
                 self.set_mode_service(custom_mode="OFFBOARD")
@@ -124,22 +113,27 @@ class DroneController:
         
         # Ждем подключения к FCU
         while not self.current_state and not rospy.is_shutdown():
+            rospy.logwarn("Ожидание подключения к MAVROS...")
             self.rate.sleep()
             
-        # Отправляем несколько setpoints перед переключением режима
+        # "Прогреваем" поток setpoints, это требование MAVROS
         pose = PoseStamped()
-        pose.pose.position.z = 1.0 # Стартовая высота [cite: 19]
+        pose.pose.position.z = 1.0 # Стартовая высота 
         for _ in range(100):
             self.setpoint_pub.publish(pose)
             self.rate.sleep()
             
-        # Включаем OFFBOARD и Arm
+        # Включаем OFFBOARD
+        rospy.loginfo("Включение режима OFFBOARD...")
         self.set_mode_service(custom_mode="OFFBOARD")
+        
+        # Армим дрон
+        rospy.loginfo("Арминг...")
         self.arm_service(True)
         
         # Ждем стабилизации на (0,0,1)
         self.go_to_point('0')
-        rospy.loginfo("Готов к миссии. (Время отсчета пошло) [cite: 19]")
+        rospy.loginfo("Готов к миссии. (Отсчет времени начинается)")
 
     def run_mission(self):
         """Выполняет полет по заданному маршруту."""
@@ -148,28 +142,25 @@ class DroneController:
             # Летим к следующей точке
             self.go_to_point(point_name)
             
-            # Выполняем действие в точке
+            # Выполняем обязательное действие в точке
             if point_name in self.pickup_points:
-                # "Захват" 
-                rospy.loginfo(f"Захват груза в {point_name}...")
-                self.set_led(True) # Включить зеленый [cite: 23]
-                rospy.sleep(3.0) # Зависание 3 сек 
+                # "Захват"
+                rospy.loginfo(f"Имитация захвата в {point_name} (3 сек)...")
+                rospy.sleep(3.0) # Обязательное зависание 
             
             elif point_name in self.dropoff_points:
-                # "Разгрузка" 
-                rospy.loginfo(f"Разгрузка груза в {point_name}...")
-                self.set_led(False) # Выключить [cite: 25]
-                rospy.sleep(3.0) # Зависание 3 сек 
+                # "Разгрузка"
+                rospy.loginfo(f"Имитация разгрузки в {point_name} (3 сек)...")
+                rospy.sleep(3.0) # Обязательное зависание 
         
-        # --- 4. Завершение миссии ---
+        # --- Завершение миссии ---
         rospy.loginfo("Миссия выполнена. Возвращение на базу...")
-        self.go_to_point('0') # Возврат в (0,0,1) [cite: 20]
+        self.go_to_point('0') # Возврат в (0,0,1) 
         
-        rospy.loginfo("Стабилизация на базе... (Окончание времени) [cite: 21]")
-        # (Можно добавить проверку стабилизации скоростей)
+        rospy.loginfo("Стабилизация на базе... (Окончание времени)")
         
         rospy.loginfo("Выполнение посадки...")
-        self.land_service(altitude=0, latitude=0, longitude=0, min_pitch=0) # Посадка в (0,0,0) [cite: 22]
+        self.land_service(altitude=0, latitude=0, longitude=0, min_pitch=0) # Посадка 
         rospy.sleep(5) # Даем время на посадку
         rospy.loginfo("Миссия завершена.")
 
